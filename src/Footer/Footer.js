75563d9f0b7a6dddb7ddc68194f6bb03e6d02b39
Avgchamp <gotovchit.99@gmail.com>
2023-07-13 09:07:08 +0300

    mainBlock

---
+++ src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Navbar = () => {
+  return(
+    <div className='footer'>
+        
+    
+
+    </div>
+  )
+}
+
+export default Navbar;
---
+++ src/Main/Main.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import Link from '../Images/link.png';
+import './Main.css';
+
+
+const Main = () => {
+  return(
+    <div className='main'>
+        
+        <div className='blockLink'>
+
+            <div className='blockLineInside'>
+                <div className='Link'>
+                    <img src={Link} alt='Gerb Republic of Belarus' />
+                    <a href='#'>pupa.com</a>
+                </div>
+
+                <div className='Link'>
+                    <img src={Link} alt='Gerb Republic of Belarus' />
+                    <a href='#'>pupa.com</a>
+                </div>
+
+                <div className='Link'>
+                    <img src={Link} alt='Gerb Republic of Belarus' />
+                    <a href='#'>pupa.com</a>
+                </div>
+
+                <div className='Link'>
+                    <img src={Link} alt='Gerb Republic of Belarus' />
+                    <a href='#'>pupa.com</a>
+                </div>
+
+                <div className='Link'>
+                    <img src={Link} alt='Gerb Republic of Belarus' />
+                    <a href='#'>pupa.com</a>
+                </div>
+
+                <div className='Link'>
+                    <img src={Link} alt='Gerb Republic of Belarus' />
+                    <a href='#'>pupa.com</a>
+                </div>
+
+                <div className='Link'>
+                    <img src={Link} alt='Gerb Republic of Belarus' />
+                    <a href='#'>pupa.com</a>
+                </div>
+            </div>
+
+
+        </div>
+       
+        <div className='phoneList'>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Main;
